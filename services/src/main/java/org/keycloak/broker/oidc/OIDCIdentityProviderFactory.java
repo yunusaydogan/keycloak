e744b0e8a1c786df5a29a39fb3aa7d9facdfd4d3
yunusaydogan <104597579+yunusaydogan@users.noreply.github.com>
2023-07-20 15:16:32 +0300

    geri alındı

--- services/src/main/java/org/keycloak/broker/oidc/OIDCIdentityProviderFactory.java
+++ services/src/main/java/org/keycloak/broker/oidc/OIDCIdentityProviderFactory.java
@@ -14,39 +14,69 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.keycloak.social.linkedin;
+package org.keycloak.broker.oidc;
 
-import org.keycloak.broker.oidc.OAuth2IdentityProviderConfig;
 import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
 import org.keycloak.models.IdentityProviderModel;
-import org.keycloak.broker.social.SocialIdentityProviderFactory;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.protocol.oidc.representations.OIDCConfigurationRepresentation;
+import org.keycloak.util.JsonSerialization;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 /**
- * @author Vlastimil Elias (velias at redhat dot com)
+ * @author Pedro Igor
  */
-public class LinkedInIdentityProviderFactory extends AbstractIdentityProviderFactory<LinkedInIdentityProvider>
-		implements SocialIdentityProviderFactory<LinkedInIdentityProvider> {
+public class OIDCIdentityProviderFactory extends AbstractIdentityProviderFactory<OIDCIdentityProvider> {
 
-	public static final String PROVIDER_ID = "linkedin";
+    public static final String PROVIDER_ID = "oidc";
 
-	@Override
-	public String getName() {
-		return "LinkedIn";
-	}
+    @Override
+    public String getName() {
+        return "OpenID Connect v1.0";
+    }
 
-	@Override
-	public LinkedInIdentityProvider create(KeycloakSession session, IdentityProviderModel model) {
-		return new LinkedInIdentityProvider(session, new OAuth2IdentityProviderConfig(model));
-	}
+    @Override
+    public OIDCIdentityProvider create(KeycloakSession session, IdentityProviderModel model) {
+        return new OIDCIdentityProvider(session, new OIDCIdentityProviderConfig(model));
+    }
 
-	@Override
-	public OAuth2IdentityProviderConfig createConfig() {
-		return new OAuth2IdentityProviderConfig();
-	}
+    @Override
+    public OIDCIdentityProviderConfig createConfig() {
+        return new OIDCIdentityProviderConfig();
+    }
 
-	@Override
-	public String getId() {
-		return PROVIDER_ID;
-	}
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public Map<String, String> parseConfig(KeycloakSession session, InputStream inputStream) {
+        return parseOIDCConfig(session, inputStream);
+    }
+
+    protected static Map<String, String> parseOIDCConfig(KeycloakSession session, InputStream inputStream) {
+        OIDCConfigurationRepresentation rep;
+        try {
+            rep = JsonSerialization.readValue(inputStream, OIDCConfigurationRepresentation.class);
+        } catch (IOException e) {
+            throw new RuntimeException("failed to load openid connect metadata", e);
+        }
+        OIDCIdentityProviderConfig config = new OIDCIdentityProviderConfig();
+        config.setIssuer(rep.getIssuer());
+        config.setLogoutUrl(rep.getLogoutEndpoint());
+        config.setAuthorizationUrl(rep.getAuthorizationEndpoint());
+        config.setTokenUrl(rep.getTokenEndpoint());
+        config.setUserInfoUrl(rep.getUserinfoEndpoint());
+        if (rep.getJwksUri() != null) {
+            config.setValidateSignature(true);
+            config.setUseJwksUrl(true);
+            config.setJwksUrl(rep.getJwksUri());
+        }
+        return config.getConfig();
+    }
+
 }
